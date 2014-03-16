de7baccc12c400ab58b0e40f3502b94dafb2cd39
mimouni <mimouni@MacBook-Pro-de-Jam.local>
2014-03-16 23:40:25 +0100

    ShellAchiev

--- ShellAchiev.sh
+++ ShellAchiev.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+
 echo "Enter your name :"
 read user_name
 
 if [ ! -f "$user_name"".log" ]
 then
-    echo "Welcome $user_name"
-    echo "Creation of your log file..."
-    touch "$user_name"".log"
+    echo "Before starting this program please execute this command : history >> $user_name.log"
+    exit
 else
-    echo "Your name is already saved on this machine !"
+    echo "Welcome $user_name"
 fi
 
 echo "Enter the folder that content your index.html :"
@@ -23,11 +23,11 @@ done
 
 echo "Ok, your index.html is linked to your log file"
 
-com_tableau=( "ls" "cd" "pwd" "cp" "mv" "touch" "mkdir" "emacs" "nano" )
+com_tableau=( "ls" "cd" "pwd" "cp" "mv" "touch" "mkdir" "emacs" "nano" "cat")
 
 for index in "${!com_tableau[@]}"
 do
-    declare c_"${com_tableau[$index]}"=`grep -w "${com_tableau[$index]}" "$user_name"".log" | cut -d " " -f3`
+    declare c_"${com_tableau[$index]}"=`grep -wc "${com_tableau[$index]}" "$user_name"".log"| cut -d " " -f1`
     var="c_"${com_tableau[$index]}
     sed -i "" s/arr_stat.${com_tableau[$index]}=[0-9]*/arr_stat.${com_tableau[$index]}=${!var}/g "$folder_name"/stats.js
 done
@@ -37,6 +37,8 @@ do
     var="c_"${com_tableau[$index]}
     declare varsucc=`grep -w "var tab_com_succ" "$folder_name"/content.js`
     declare vardef=`grep -w "var tab_com_def" "$folder_name"/content.js`
+    declare varsucc2=`grep -w "var tab_com_succ" "$folder_name"/content2.js`
+    declare vardef2=`grep -w "var tab_com_def" "$folder_name"/content2.js`
 
     if [ ! -z ${!var} ]
     then
@@ -47,7 +49,13 @@ do
               sed -i "" s/\"${com_tableau[$index]}\",\ \/""/g "$folder_name"/content.js
               sed -i "" s/var\ \tab_com_def\ \=\ \new\ \Array\(/var\ \tab_com_def\ \=\ \new\ \Array\(\"${com_tableau[$index]}\",\ \/g "$folder_name"/content.js 
             fi
+            if grep -q \"${com_tableau[$index]}\" <<< $varsucc2
+            then
+              sed -i "" s/\"${com_tableau[$index]}\",\ \/""/g "$folder_name"/content2.js
+              sed -i "" s/var\ \tab_com_def\ \=\ \new\ \Array\(/var\ \tab_com_def\ \=\ \new\ \Array\(\"${com_tableau[$index]}\",\ \/g "$folder_name"/content2.js 
+            fi
         fi
+    fi
 	if [ ${!var} -ge "1" ]
 	then
 	    if grep -q \"${com_tableau[$index]}\" <<< $vardef
@@ -55,7 +63,17 @@ do
 	      sed -i "" s/\"${com_tableau[$index]}\",\ \/""/g "$folder_name"/content.js
               sed -i "" s/var\ \tab_com_succ\ \=\ \new\ \Array\(/var\ \tab_com_succ\ \=\ \new\ \Array\(\"${com_tableau[$index]}\",\ \/g "$folder_name"/content.js 
               echo "Good job, you've just unlocked a new achievement : Make your first ${com_tableau[$index]}"
-            fi
+        fi
+    fi
+    if [ ${!var} -ge "10" ]
+    then
+        if grep -q \"${com_tableau[$index]}\" <<< $vardef2
+        then
+          sed -i "" s/\"${com_tableau[$index]}\",\ \/""/g "$folder_name"/content2.js
+              sed -i "" s/var\ \tab_com_succ\ \=\ \new\ \Array\(/var\ \tab_com_succ\ \=\ \new\ \Array\(\"${com_tableau[$index]}\",\ \/g "$folder_name"/content2.js 
+              echo "Good job, you've just unlocked a new achievement : Make ten ${com_tableau[$index]}"
         fi
     fi
 done
+
+rm "$user_name"".log"
